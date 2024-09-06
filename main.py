d35b50f2f48b418ee6e1ad7da238c0b7d39b9069
BluePy <bluepythewolfdeer@proton.me>
2024-09-06 17:55:32 -0600

    The uploading lel

---
+++ main.py
@@ -0,0 +1,19 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from PySide6.QtWidgets import QApplication
+
+def(AppOpening):
+    if app.exec  = 1:
+        print("Welcome!")
+        else:
+            return 0;
+
+AppOpening()
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # ...
+    sys.exit(app.exec())
